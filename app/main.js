502b8964474a441df5fc43bc3713e5344020e10c
Flaviana Ferraz <113718720+FlavianaFXT@users.noreply.github.com>
2024-01-18 16:05:44 +0000

    main.js

---
+++ app/main.js
@@ -0,0 +1,11 @@
+let livros = [];
+
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+getBuscarLivrosDaApi();
+
+async function getBuscarLivrosDaApi(){
+    const res = await fetch(endpointDaAPI);
+    livros = await res.json();
+    console.log(livros);
+}
