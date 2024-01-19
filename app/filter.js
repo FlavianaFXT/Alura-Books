4aa63d6c7109df5f4ab6eec079dd17ccfe87493f
Flaviana Ferraz <113718720+FlavianaFXT@users.noreply.github.com>
2024-01-19 19:05:04 +0000

    filter

---
+++ app/filter.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll('.btn');
+
+botoes.forEach(btn => btn.addEventListener('click, filtrarLivros'))
+
+function filtrarLivros(){
+    const eletoBtn = document.getElementById(this.id)
+    const categoria = eletoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    console.table(livrosFiltrados)
+}
